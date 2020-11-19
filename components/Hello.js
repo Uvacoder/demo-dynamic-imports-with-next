37c827b73f661b2197235fabcf903ec3a57d41cb
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-11-19 11:06:25 +1100

    feat: completed basic example

---
+++ components/Hello.js
@@ -0,0 +1,7 @@
+export function Hello({ name }) {
+  return <p>Hello, {name}!</p>;
+}
+
+export function Goodbye({ name }) {
+  return <p>Goodbye, {name}!</p>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,38 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
 
-export default function Home() {
+const DynamicHello = dynamic(() =>
+  import('../components/Hello').then((mod) => mod.Hello),
+);
+
+const DynamicGoodbye = dynamic(() =>
+  import('../components/Hello').then((mod) => mod.Goodbye),
+);
+
+function Home() {
+  const [modules, addModule] = useState([]);
+
+  const addHello = () =>
+    addModule([
+      ...modules,
+      { Component: DynamicHello, props: { name: 'World!' } },
+    ]);
+
+  const addGoodbye = () =>
+    addModule([
+      ...modules,
+      { Component: DynamicGoodbye, props: { name: 'Cruel World!' } },
+    ]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      {modules.map(({ Component, props }) => (
+        <Component {...props} />
+      ))}
+      <button onClick={addHello}>Add hello!</button>
+      <button onClick={addGoodbye}>Add Goodbye!</button>
     </div>
-  )
+  );
 }
+
+export default Home;
